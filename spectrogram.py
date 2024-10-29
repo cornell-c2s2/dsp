79a1ee9e5a33704e63cbae0c0e5801df54cdc3d1
Jack Frank <jf679@cornell.edu>
2024-10-29 15:08:34 -0400

    doFilter

--- spectrogram.py
+++ spectrogram.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.signal import spectrogram, butter, lfilter
-audioFiles = [ "1363v2.WAV"]
+audioFiles = [ "1363v2.WAV", "1809v2.WAV", "2237v2.WAV"]
+doFilter = True
 for i in audioFiles:
     audioFile = "audio/"+i
 
@@ -29,34 +30,34 @@ for i in audioFiles:
     plt.colorbar(label='Intensity [dB]')
     plt.ylim(0, 7000)
     plt.show()
+    if doFilter:
+        # Define Butterworth band-pass filter
+        def butter_bandpass(lowcut, highcut, fs, order=4):
+            nyquist = 0.5 * fs
+            low = lowcut / nyquist
+            high = highcut / nyquist
+            b, a = butter(order, [low, high], btype='band')
+            return b, a
 
-    # Define Butterworth band-pass filter
-    def butter_bandpass(lowcut, highcut, fs, order=4):
-        nyquist = 0.5 * fs
-        low = lowcut / nyquist
-        high = highcut / nyquist
-        b, a = butter(order, [low, high], btype='band')
-        return b, a
+        def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
+            b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+            return lfilter(b, a, data)
 
-    def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
-        b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-        return lfilter(b, a, data)
+        # Frequency ranges for filters
+        bands = [(1500, 3750), (1500, 2250), (2250, 3000), (3000, 3750)]
+        threshold_dB = -75
 
-    # Frequency ranges for filters
-    bands = [(1500, 3750), (1500, 2250), (2250, 3000), (3000, 3750)]
-    threshold_dB = -75
-
-    for lowcut, highcut in bands:
-        filtered_signal = butter_bandpass_filter(mySoundOneChannel, lowcut, highcut, samplingFreq)
-        frequencies, times, Sxx = spectrogram(filtered_signal, fs=samplingFreq)
-        Sxx = 10 * np.log10(Sxx)
-        Sxx_dB_filtered = np.where(Sxx > threshold_dB, Sxx, np.nan)
-        
-        plt.figure(figsize=(10, 4))
-        plt.pcolormesh(times, frequencies, Sxx_dB_filtered, shading='gouraud')
-        plt.ylabel('Frequency [Hz]')
-        plt.xlabel('Time [s]')
-        plt.title(f'Spectrogram (Band-Pass {lowcut}-{highcut}) of {i}')
-        plt.colorbar(label='Intensity [dB]')
-        plt.ylim(0, 7000)
-        plt.show()
+        for lowcut, highcut in bands:
+            filtered_signal = butter_bandpass_filter(mySoundOneChannel, lowcut, highcut, samplingFreq)
+            frequencies, times, Sxx = spectrogram(filtered_signal, fs=samplingFreq)
+            Sxx = 10 * np.log10(Sxx)
+            Sxx_dB_filtered = np.where(Sxx > threshold_dB, Sxx, np.nan)
+            
+            plt.figure(figsize=(10, 4))
+            plt.pcolormesh(times, frequencies, Sxx_dB_filtered, shading='gouraud')
+            plt.ylabel('Frequency [Hz]')
+            plt.xlabel('Time [s]')
+            plt.title(f'Spectrogram (Band-Pass {lowcut}-{highcut}) of {i}')
+            plt.colorbar(label='Intensity [dB]')
+            plt.ylim(0, 7000)
+            plt.show()
