bf39a62db518ca73c266da996d22f7a8ca589dcb
Jack Frank <jackfrank@dhcp-vl2041-33693.eduroam.cornell.edu>
2025-10-21 12:03:10 -0400

    instructions

--- 2fa/speaker_verify_mfcc.py
+++ 2fa/speaker_verify_mfcc.py
@@ -1,3 +1,6 @@
+# To train: python3 speaker_verify_mfcc.py train \ --pos data/pos --neg data/neg \ --min_precision 0.98
+# To run: python3 speaker_verify_mfcc.py predict --folder testing
+
 #!/usr/bin/env python3
 import os, glob, json, argparse, warnings
 import numpy as np
